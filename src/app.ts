7f0f64a23f7ab52fe84a44b9f1616ca82079719e
BentlyM <metayerbently@gmail.com>
2024-09-13 22:53:03 -0400

    for deployment.

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import passport from 'passport';
 const app = express();
 
 const PORT = +(process.env.PORT || 8080);
-const hostname = '127.0.0.9';
+const hostname = '0.0.0.0';
 
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
 app.use(passport.session());
